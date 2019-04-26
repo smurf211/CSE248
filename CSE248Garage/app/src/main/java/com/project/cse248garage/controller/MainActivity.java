ed4bfcae74fdb93008fdd64af9aafca832dd26f3
Your Name <you@example.com>
2019-04-25 23:06:39 -0400

    working on toolbar

--- CSE248Garage/app/src/main/java/com/project/cse248garage/controller/MainActivity.java
+++ CSE248Garage/app/src/main/java/com/project/cse248garage/controller/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -38,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
 
 
